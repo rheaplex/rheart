78054737917ea3ec453e0b136f010d9dffb941b2
Rhea Myers <rhea@myers.studio>
2023-04-10 18:00:31 -0700

    Trivial cleanup to ensure ae runs.

--- ae/run.lisp
+++ ae/run.lisp
@@ -16,7 +16,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(load "../library/ptester.lisp")
+;;(load "../library/ptester.lisp")
 
 (load "./ae.lisp")
 
